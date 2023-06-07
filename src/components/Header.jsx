cf4e63ba0a5635621caa631c3e3cc00854efd15d
Roberto Núñez <Roberto>
2023-06-06 22:53:31 -0500

    Header finalizado

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import { HashLink } from "react-router-hash-link"
+import { useEffect, useState } from "react"
+
+import Logo from "../assets/icons/logo.svg"
+import Menu from "../assets/icons/menu.svg"
+
+const Header = () => {
+
+    const [ visibleMenu, setVisibleMenu ] = useState( false );
+
+    const hiddenNavbar = () => {
+        setVisibleMenu(!visibleMenu);
+    }
+
+    useEffect(()=>{
+      
+      function visible(){
+        if(window.innerWidth>768){
+          setVisibleMenu(false);
+        }
+      }
+
+      window.addEventListener("resize",visible);
+
+    },[window])
+
+    return (
+        <div className="flex flex-col items-center gap-3 py-6 px-9 bg-[#1E1A1A] md:flex-row justify-between">
+            <p className="text-xl text-white text-center font-bold relative">
+                <img className="absolute" src={ Logo } alt="Logo" />
+                <span className="block text-xl text-[#FF6600]">
+                    BIMARQ3D
+                </span>
+                Architecture Studio
+            </p>
+            <button onClick={ hiddenNavbar } className="md:hidden">
+                <img src={ Menu } alt="Menú bars" />
+            </button>
+            <div className={`flex flex-col items-center gap-3 md:visible ${ visibleMenu? "hidden" : "" } md:flex-row md:gap-4`}>
+                <HashLink smooth to="/#estudios" className="text-lg text-white hover:text-[#FF6600] ">Estudios</HashLink>
+                <HashLink smooth to="/#quienesomos" className="text-lg text-white hover:text-[#FF6600] ">Quiénes Somos</HashLink>
+                <HashLink smooth to="/#proyectos" className="text-lg text-white hover:text-[#FF6600] ">Proyectos</HashLink>
+                <HashLink smooth to="/#servicios" className="text-lg text-white hover:text-[#FF6600] ">Servicios</HashLink>
+                <HashLink smooth to="/#testimonios" className="text-lg text-white hover:text-[#FF6600] ">Testimonios</HashLink>
+                <HashLink smooth to="/#contactenos" className="text-lg text-white hover:text-[#FF6600] ">Contáctenos</HashLink>
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import { BrowserRouter as Router } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Router>
+        <App />
+    </Router>
+)
