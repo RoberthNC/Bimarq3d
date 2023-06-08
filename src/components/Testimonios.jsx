4552c4ef65a874a5e993d2fe51d88f3b4576d7f3
Roberto Núñez <robertonc1599@gmail.com>
2023-06-08 09:38:09 -0500

    Configuraciones al diseño de los Testimonios

--- src/components/Testimonios.jsx
+++ src/components/Testimonios.jsx
@@ -4,7 +4,7 @@ import { testimonios } from "../data/testimonios.js"
 
 const Testimonios = () => {
   return (
-    <div id="testimonios" className="flex flex-col items-center gap-3 py-6 px-9 mb-3">
+    <div id="testimonios" className="flex flex-col items-center mx-auto gap-3 py-6 px-9 mb-3 lg:w-[55%]">
         <h2 className="text-xl text-center text-[#FF6600] font-bold">Testimonios</h2>
         <p className="text-lg text-center mb-3">¿Qué dicen de BIMARQ3D?</p>
         <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-6 md:gap-3">
