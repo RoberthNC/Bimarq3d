c964ac7f985b05ca24fe77369110783e2ab1f106
Roberto Núñez <robertonc1599@gmail.com>
2023-06-17 17:26:22 -0500

    Versión estable de sliders

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,5 +1,4 @@
 import { Swiper, SwiperSlide } from "swiper/react"
-import { FreeMode } from "swiper"
 import "swiper/css"
 import "swiper/css/free-mode"
 import SwiperCore, { Navigation, Pagination } from "swiper/core"
